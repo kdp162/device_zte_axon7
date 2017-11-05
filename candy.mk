90de7b0b8e2640e9a3d4affdfb952b5857285163
Kevin Princiotta <kevinprinciotta@gmail.com>
2017-11-05 09:18:47 -0500

    Candify

--- candy.mk
+++ candy.mk
@@ -15,10 +15,10 @@
 
 $(call inherit-product, device/zte/axon7/full_axon7.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Candy stuff.
+$(call inherit-product, vendor/candy/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_axon7
+PRODUCT_NAME := candy_axon7
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="P996A01_N" \
